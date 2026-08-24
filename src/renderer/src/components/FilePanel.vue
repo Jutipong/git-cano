919b7ff1a687bccbb02639e552a1d8bf7f2c9f3c
Jutipong.Dev <techcom.ec20@gmail.com>
2026-08-24 17:31:21 +0700

    Increase commit message box to 6 rows

--- src/renderer/src/components/FilePanel.vue
+++ src/renderer/src/components/FilePanel.vue
@@ -306,13 +306,13 @@
                 v-if="mode === 'commit'"
                 :value="commitSubject"
                 placeholder="No commit message"
-                rows="4"
+                rows="6"
                 readonly />
             <textarea
                 v-else
                 v-model="message"
                 placeholder="Summary of changes"
-                rows="4"
+                rows="6"
                 @keydown.enter.meta.prevent="doCommit()"
                 @keydown.enter.ctrl.prevent="doCommit()" />
             <button
