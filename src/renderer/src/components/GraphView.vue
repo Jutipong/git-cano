eb68fccfc463aaa11e50c2f0ecd4f5f1f56b6076
T660046 <24461402+Jutipong@users.noreply.github.com>
2026-09-03 16:16:42 +0700

    fix: prefer author name over email for avatar keys

--- src/renderer/src/components/GraphView.vue
+++ src/renderer/src/components/GraphView.vue
@@ -302,7 +302,7 @@
     }
 
     function avatarKey(commit: CommitNode): string {
-        return commit.authorEmail || commit.author
+        return commit.author || commit.authorEmail
     }
 
     function avatarInitial(commit: CommitNode): string {
